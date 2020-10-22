222d29aa17535acee7892fd6185554206d498333
fintank <finapptank@gmail.com>
2020-10-22 01:44:33 -0400

    installed inquirer

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 // Refactored Code
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  // inquirer receives type, name, messages etc that can be prompted for
 
 
 
-fs.writeFile('./home.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+
+
+
+
+//old version using process.argv
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+
+
+// fs.writeFile('./home.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -19,3 +19,13 @@ const generatePage = (name, github) => {
 
 
   module.exports = generatePage;
+
+
+  // node package manager is an online repo for publishing open source code
+  // use https://www.npmjs.com/search?q=command%20line%20prompt to search for npm packages
+  // to get to the npm command line utility we type npm init, install, uninstall packages, etc
+//   We'll create a package.json file to serve as the manifest for the project. 
+//   The information in this file pertains to the metadata of the project and which npm packages are being used—called dependencies. 
+//   The package.json file also serves several other purposes that we'll cover later.
+// typing npm install will install dependencies cloned from a repo
+// git rm -r --cached node_modules this removes tracking of files we would want ignored
